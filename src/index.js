01ef8f3647dabb303ba08696cffb9a4b610136c4
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-04-03 23:37:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,25 +20,21 @@ async function handleSubmitBtnClick(e) {
 
   refs.galleryEl.innerHTML = '';
 
-  try {
-    const searchQuery = e.target.elements['searchQuery'].value.trim();
-    const searchResults = await fetchPics(searchQuery);
+  const searchQuery = e.target.elements['searchQuery'].value.trim();
+  const searchResults = await fetchPics(searchQuery);
 
-    const imageArray = searchResults.data.hits;
+  const imageArray = searchResults.data.hits;
 
-    if (imageArray.length === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      return;
-    }
-
-    const galleryMarkup = makeGalleryMarkup(imageArray);
-
-    refs.galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
-  } catch (error) {
-    console.log(error);
+  if (imageArray.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
   }
+
+  const galleryMarkup = makeGalleryMarkup(imageArray);
+
+  refs.galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
 
   refs.searchForm.reset();
 
